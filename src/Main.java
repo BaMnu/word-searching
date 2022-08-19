23b13a8a4b8ffe26fe0140d09a56ab80bb79d551
Olga Fedorova <bamnu13@gmail.com>
2022-08-19 13:53:10 +0300

    Updated output

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://hsqldb.org/doc/2.0/changelist_2_0.txt");
-        BufferedReader read = new BufferedReader(new InputStreamReader(url.openStream()));
+        Scanner scanner = new Scanner(System.in);
+//  ссылка на текстовый документ для примера - https://hsqldb.org/doc/2.0/changelist_2_0.txt
 
+        System.out.println("Please insert desired link with text file:");
+        WordsChecker checkWord = new WordsChecker(new URL(scanner.nextLine()));
 
+        System.out.println("Insert the word to find:");
+        String search = scanner.nextLine().toLowerCase();
+        System.out.println("Searching...");
+        checkWord.hasWord(search);
     }
 }
